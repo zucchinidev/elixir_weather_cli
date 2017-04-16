f68dd7ea08d0270ee5d127010cd093c3bda3236d
Andrea Zucchini <azga84@gmail.com>
2017-04-16 22:51:01 +0200

    parse cities

---
+++ lib/weather_cli/parse_cities.ex
@@ -0,0 +1,35 @@
+defmodule WeatherCli.ParseCities do
+
+  def init_search(query) do
+    find_city(query)
+  end
+  
+  def find_city(query) do
+  File.stream!(get_file_path())
+    |> Enum.find(&find_city_from_line_file(&1, query))
+    |> handle_find
+  end
+
+  def find_city_from_line_file(line, city) do
+    map_city = Poison.Parser.parse!(line)
+    String.downcase(city) == String.downcase(map_city["name"])
+  end
+
+  defp handle_find(nil), do: IO.puts "City not found"
+  defp handle_find(city) do
+    city
+      |> Poison.Parser.parse
+      |> handle_parse
+  end
+
+  defp handle_parse({:ok, city}), do: IO.inspect city
+  defp handle_parse({:error, _}) do
+    raise "Upps, an error has occurred"
+  end
+
+  defp get_file_path do
+    root_dir = File.cwd!()
+    data_dir = Path.join(~w(#{root_dir} lib weather_cli data))
+    Path.join(~w(#{data_dir} city.list.json))
+  end
+end
