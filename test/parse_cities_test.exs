9477d8bb31e744870fffe0425550d9ea69c48979
Andrea Zucchini <azga84@gmail.com>
2017-04-16 23:48:45 +0200

    cities test

--- test/parse_cities_test.exs
+++ test/parse_cities_test.exs
@@ -1,21 +1,51 @@
-defmodule WeatherCliTest do
+defmodule WeatherCli.ParseCitiesTest do
   use ExUnit.Case
-  doctest WeatherCli
-  import WeatherCli.CLI, only: [
-    parser_args: 1,
-    get_city_id_from_city_name: 1
+  import ExUnit.CaptureIO
+  import WeatherCli.ParseCities, only: [
+    find_city: 1,
+    find_city_from_line_file: 2,
+    handle_find: 1,
+    handle_parse: 1,
+    get_file_path: 0
   ]
 
-  test ":help returned by option parsing with -h and --help options" do
-    assert parser_args(["-h", "anything"]) == :help
-    assert parser_args(["--help", "anything"]) == :help
+  test "show message City not found with option with nil value" do
+    result = capture_io(fn ->
+      handle_find(nil)
+    end)
+
+    assert result == "City not found\n"
   end
 
-  test "one values returned if one given" do
-    assert parser_args(["madrid"]) == { "madrid" }
+
+  test "A run-time error is thrown by inserting an invalid json" do
+    invalid_json = get_mock_city_in_string_format() <> "{{ddd}}"
+    assert_raise RuntimeError, "Upps, an error has occurred", fn ->
+      handle_find(invalid_json)
+    end
   end
 
-  test "transform city name to city id" do
-    assert get_city_id_from_city_name({ "madrid" }) == {"madrid"}
+  test "a city is returned when we pass as a valid json parameter" do
+    assert handle_find(get_mock_city_in_string_format()) == get_mock_city()
+  end
+
+  test "a city is returned when we search a valid city" do
+    assert find_city("madrid") == get_mock_city()
+  end
+
+  defp get_mock_city do
+    %{
+      "name" => "Madrid",
+      "country" => "ES",
+      "_id" => 6359304,
+      "coord" => %{
+                    "lat" => 40.489349,
+                    "lon" => -3.68275
+                 }
+    }
+  end
+
+  defp get_mock_city_in_string_format do
+    "{\"name\":\"Madrid\",\"country\":\"ES\",\"coord\":{\"lon\":-3.68275,\"lat\":40.489349},\"_id\":6359304}"
   end
 end
