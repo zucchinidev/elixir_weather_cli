2b9208abf2e03ce80791167c782bb2beb8b90c48
Andrea Zucchini <azga84@gmail.com>
2017-04-16 15:17:24 +0200

    test parse args

--- test/weather_cli_test.exs
+++ test/weather_cli_test.exs
@@ -1,8 +1,21 @@
 defmodule WeatherCliTest do
   use ExUnit.Case
   doctest WeatherCli
+  import WeatherCli.CLI, only: [
+    parser_args: 1,
+    get_city_id_from_city_name: 1
+  ]
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test ":help returned by option parsing with -h and --help options" do
+    assert parser_args(["-h", "anything"]) == :help
+    assert parser_args(["--help", "anything"]) == :help
+  end
+
+  test "one values returned if one given" do
+    assert parser_args(["madrid"]) == { "madrid" }
+  end
+
+  test "transform city name to city id" do
+    assert get_city_id_from_city_name({ "madrid" }) == {"madrid"}
   end
 end
