4ae72a177addcea6513d4c1fc14731ff4c3fec7c
Andrea Zucchini <azga84@gmail.com>
2017-04-17 01:09:57 +0200

    add api key in config app

--- config/config.exs
+++ config/config.exs
@@ -10,7 +10,7 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :weather_cli, key: :value
+config :weather_cli, API_KEY: System.get_env("WEATHER_API_KEY")
 #
 # And access this configuration in your application as:
 #
