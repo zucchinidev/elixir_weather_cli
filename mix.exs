4560411a3e9ecc2728628cb83da9c1896c1e9369
Andrea Zucchini <azga84@gmail.com>
2017-04-17 02:40:30 +0200

    add http poison

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule WeatherCli.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -29,7 +29,8 @@ defmodule WeatherCli.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 0.11.1"}
     ]
   end
 end
